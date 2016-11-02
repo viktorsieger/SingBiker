e4a9360fe7a412b3744fc9f63ddafb30f97982e4
Viktor Sieger <viktorsieger@users.noreply.github.com>
2016-11-02 23:35:53 +0800

    VS *Added clear button (in action bar) to RoutePlotActivity.

--- app/src/main/java/sg/ntu/dataminers/singbiker/boundary/RoutePlotActivity.java
+++ app/src/main/java/sg/ntu/dataminers/singbiker/boundary/RoutePlotActivity.java
@@ -115,14 +115,11 @@ public class RoutePlotActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_clear_input) {
+            startSearchBar.setText("");
+            endSearchBar.setText("");
         }
 
         return super.onOptionsItemSelected(item);
